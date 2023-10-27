f2d672b273edbeeaacc9f965af63d2ea83871810
Marion <lamacq.marion@hotmail.fr>
2023-10-27 13:49:56 +0200

    Main implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -3,27 +3,25 @@ package com.hemebiotech.analytics;
 import java.io.*;
 import java.util.Map;
 
-
 public class WriteSymptomDataToFile implements ISymptomWriter {
 
-    private String filepathoutput;
+    private final String filepathOutput;
 
-    public WriteSymptomDataToFile(String filepathoutput) {
-        this.filepathoutput = filepathoutput;
+    public WriteSymptomDataToFile(final String filepathOutput) {
+
+        this.filepathOutput = filepathOutput;
     }
 
     @Override
     public void writeSymptoms(Map<String, Integer> symptoms) {
+            try(final FileWriter fileWriter = new FileWriter(filepathOutput) ) {
 
-        try(final FileWriter fileWriter = new FileWriter("result.out") ) {
-
-            for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
-                fileWriter.write(entry.getKey() + " : " + entry.getValue() + "\n");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
+                for (Map.Entry<String, Integer> entry : symptoms.entrySet()) {
+                    fileWriter.write(entry.getKey() + " : " + entry.getValue() + "\n");
+                }
+            } catch (IOException e){
+                System.out.println("An error occurred.");
+                e.printStackTrace();
         }
-
     }
 }
